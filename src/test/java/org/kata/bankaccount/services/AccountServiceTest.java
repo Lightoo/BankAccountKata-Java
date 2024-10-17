1402d688716361e8f96ca0190b4c6321b66088b0
Abdellatif AZIKI <abdellatifaziki@gmail.com>
2024-10-17 11:41:04 +0200

    US 2: add 2 unit tests withdraw account

--- src/test/java/org/kata/bankaccount/services/AccountServiceTest.java
+++ src/test/java/org/kata/bankaccount/services/AccountServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.kata.bankaccount.model.Account;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -27,6 +29,20 @@ public class AccountServiceTest {
         Account account = new Account();
         assertThrows(Exception.class,()->{
             accountService.deposit(account,-200);
+        });
+    }
+    @Test
+    void withdraw_with_sufficient_amount() throws Exception {
+        Account account = new Account(2000, new ArrayList<>());
+        accountService.withdraw(account,500);
+        assertEquals(1500, account.getBalance());
+        assertEquals(1, account.getTransactions().size());
+    }
+    @Test
+    void withdraw_with_insufficient_amount() {
+        Account account = new Account(1000, new ArrayList<>());
+        assertThrows(Exception.class,()->{
+            accountService.withdraw(account,2000);
         });
     }
 }
